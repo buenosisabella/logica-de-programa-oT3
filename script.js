eaa9e430858913355f3b49ef2f08de8b41a17a6b
buenosisabella <coreamilisabella@gmail.com>
2022-01-12 17:23:43 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Olá,mundo")
+
+let name= "Isabella"
+
+console.log(name)
+
+let number= 26
+let ano= 2022 
+
+console.log(ano- number)
+
+console.log("estou mexendo no console.log")
+
+let quartaFeira= true
+let quintafeira= false
+
+console.log(quartaFeira>quintafeira)
+
